99cc537c8b7a3c15bdf628831028c0104c07b98b
Kavirubc <hapuarachchikaviru@gmail.com>
2025-06-25 13:11:22 +0530

    style: format ecosystem.config.js for improved readability

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  apps: [{
-    name: 'sparks-app',
-    script: './node_modules/.bin/next',
-    args: 'start',
-    cwd: '/root/sparks-nextjs',
-    instances: 1,
-    exec_mode: 'fork',
-    env: {
-      NODE_ENV: 'development',
-      PORT: 3000
-    },
-    env_production: {
-      NODE_ENV: 'production',
-      PORT: 3000
-    },
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_file: './logs/combined.log',
-    time: true,
-    watch: false,
-    max_memory_restart: '1G',
-    node_args: '--max-old-space-size=1024'
-  }]
+    apps: [{
+        name: 'sparks-app',
+        script: './node_modules/.bin/next',
+        args: 'start',
+        cwd: '/root/sparks-nextjs',
+        instances: 1,
+        exec_mode: 'fork',
+        env: {
+            NODE_ENV: 'development',
+            PORT: 3000
+        },
+        env_production: {
+            NODE_ENV: 'production',
+            PORT: 3000
+        },
+        error_file: './logs/err.log',
+        out_file: './logs/out.log',
+        log_file: './logs/combined.log',
+        time: true,
+        watch: false,
+        max_memory_restart: '1G',
+        node_args: '--max-old-space-size=1024'
+    }]
 };
